c2894ee287f36e2ac791f969a43fb23e85ce0640
Luard <luard.developer@gmail.com>
2025-04-28 08:23:31 +0000

    Sync from repo A

--- target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/astro.config.mjs
+++ target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { defaultLang } from './src/i18n/defaultLang';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'http://elbodorrio2025.com',
     i18n: {
         locales: ['es', 'ca'],
-        defaultLocale: 'es',
+        defaultLocale: defaultLang,
         routing: {
             prefixDefaultLocale: false,
             redirectToDefaultLocale: true,
